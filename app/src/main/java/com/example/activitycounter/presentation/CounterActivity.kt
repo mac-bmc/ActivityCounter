3cfea6a305549a4b9aba7f02202fe3cbd986afdd
Manu Mohan <manubmc71@gmail.com>
2025-03-02 21:59:00 +0530

    Add permissions

--- app/src/main/java/com/example/activitycounter/presentation/CounterActivity.kt
+++ app/src/main/java/com/example/activitycounter/presentation/CounterActivity.kt
@@ -1,12 +1,18 @@
 package com.example.activitycounter.presentation
 
+import android.Manifest
+import android.annotation.SuppressLint
 import android.content.Intent
+import android.content.pm.PackageManager
 import android.os.Build
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.core.content.ContextCompat
 import androidx.lifecycle.ViewModelProvider
 import com.example.activitycounter.service.TapCounterService
 import com.example.activitycounter.ui.theme.ActivityCounterTheme
@@ -15,11 +21,21 @@ import com.example.activitycounter.ui.theme.ActivityCounterTheme
 class CounterActivity : ComponentActivity() {
     private lateinit var counterViewModel: CounterViewModel
     private lateinit var serviceIntent: Intent
+    private val notificationPermission =
+        registerForActivityResult(ActivityResultContracts.RequestPermission()) { isGranted ->
+            if (isGranted) {
+                Toast.makeText(this, "Notification permission granted", Toast.LENGTH_SHORT).show()
+            } else {
+                Toast.makeText(this, "Notification permission denied!", Toast.LENGTH_SHORT).show()
+            }
+        }
 
 
+    @SuppressLint("InlinedApi")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         counterViewModel = ViewModelProvider(this)[CounterViewModel::class.java]
+        notificationPermission.launch(Manifest.permission.POST_NOTIFICATIONS)
         enableEdgeToEdge()
         setContent {
             ActivityCounterTheme {
@@ -27,6 +43,7 @@ class CounterActivity : ComponentActivity() {
             }
         }
     }
+
 
     override fun onResume() {
         if (::serviceIntent.isInitialized) {
@@ -36,15 +53,21 @@ class CounterActivity : ComponentActivity() {
     }
 
     override fun onStop() {
-        if (counterViewModel.isTracking.value == true) {
-            serviceIntent = Intent(this, TapCounterService::class.java)
-            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-                startForegroundService(serviceIntent)
-            } else {
-                startService(serviceIntent)
+        if (ContextCompat.checkSelfPermission(
+                this, Manifest.permission.POST_NOTIFICATIONS
+            ) == PackageManager.PERMISSION_GRANTED
+        ) {
+            if (counterViewModel.isTracking.value == true) {
+                serviceIntent = Intent(this, TapCounterService::class.java)
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                    startForegroundService(serviceIntent)
+                } else {
+                    startService(serviceIntent)
+                }
             }
+        } else {
+            Toast.makeText(this, "Notification permission not granted", Toast.LENGTH_SHORT).show()
         }
-
         super.onStop()
     }
 
